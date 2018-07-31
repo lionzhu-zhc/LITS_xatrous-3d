0902e1f934a6d361822f5139e9f5b756dd1547de
lionzhu-dipAI <zhuhaichen55@163.com>
2018-07-31 09:02:02 +0800

    Mod densefc

--- ISSeg/net/dense_fc.py
+++ ISSeg/net/dense_fc.py
@@ -7,18 +7,32 @@ import tensorflow as tf
 
 NUMPOOL = 4
 FIRST_LAYER_FILTERS = 48
+growth_rate
 
 def build_dense_fc(tensor_in, BN_FLAG, BATCHSIZE, CLASSNUM, IMGCHANNEL):
     print(tensor_in.get_shape())
 
+    layers_per_block = [5] * (2 * NUMPOOL + 1)
     first_layer = tf.nn.conv2d(tensor_in, filter= [3,3,IMGCHANNEL, FIRST_LAYER_FILTERS], strides= [1,1,1,1], padding= 'SAME', name= 'First_Conv')
 
     n_filters = FIRST_LAYER_FILTERS
     
+def bottleneck_layer(in_put, growth_rate, keep_prob= 0.8, name):
+    with tf.name_scope(name):
+        shap = in_put.get_shape()
+        in_channel = shap[-1]
+        res = BN_Relu_Conv(in_put, in_channel, growth_rate*4, keep_prob, BN_FLAG, kernel_size= 1) 
+        res = tf.nn.dropout(res, keep_prob)
+        res = BN_Relu_Conv(in_put, growth_rate*4, growth_rate, keep_prob, BN_FLAG, kernel_size= 3)
+        res = tf.nn.dropout(res, keep_prob)
+        return res
+
+def dense_block(in_put, n_layers, growth_rate, name):
+    with tf.name_scope(name):
+        layer_concat = list()
+        layer_concat.append(in_put)
 
 
-def dense_block(in_put, growth_rate):
-    
 
 
 def BN_Relu_Conv(in_put, in_channel, out_channel, keep_prob, BN_FLAG, kernel_size = 3, name = 'BN_Relu_Conv'):
