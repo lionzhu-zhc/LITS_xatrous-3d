b97b1a51cf5ebb99bd97f47a08c24ccaef7c8915
lionzhu-zhc <zhuhaichen55@163.com>
2018-08-02 16:38:17 +0800

    mod dense_fc

--- ISSeg/net/dense_fc.py
+++ ISSeg/net/dense_fc.py
@@ -13,7 +13,7 @@ LAYERS_PER_BLOCK = [5] * (2 * NUMPOOL + 1)
 def build_dense_fc(tensor_in, BN_FLAG, BATCHSIZE, CLASSNUM, IMGCHANNEL, keep_prob):
     print(tensor_in.get_shape())
     # the first conv layer
-    stack = tf.nn.conv2d(tensor_in, filter= [3,3,IMGCHANNEL, FIRST_LAYER_FILTERS], strides= [1,1,1,1], padding= 'SAME', name= 'First_Conv')
+    stack = conv2d_layer(kernel_size= 3, in_put= tensor_in, in_channel= IMGCHANNEL, out_channel= FIRST_LAYER_FILTERS, name= 'First_Conv')
 
     n_filters = FIRST_LAYER_FILTERS
 
@@ -23,59 +23,59 @@ def build_dense_fc(tensor_in, BN_FLAG, BATCHSIZE, CLASSNUM, IMGCHANNEL, keep_pro
     for i in range(NUMPOOL):
         for j in range(LAYERS_PER_BLOCK[i]):
             # here ignore the bottle layer of the ori densenet
-            l = BN_Relu_Conv(stack, GROWTH_RATE, keep_prob= 0.8, BN_FLAG= BN_FLAG, kernel_size= 3)
+            l = BN_Relu_Conv(stack, GROWTH_RATE, keep_prob= 0.8, BN_FLAG= BN_FLAG, kernel_size= 3, name= 'DownConv_{}_{}'.format(i,j))
             stack = tf.concat([stack, l], axis =3, name= 'Concat')
             n_filters = n_filters + GROWTH_RATE
         skip_list.append(stack)
-        stack = down_layer(stack, n_filters, keep_prob= 0.8, BN_FLAG= BN_FLAG)
+        stack = down_layer(stack, n_filters, keep_prob= 0.8, BN_FLAG= BN_FLAG, name= 'Down_{}'.format(i))
 
     skip_list = skip_list[::-1]       # reverse the order of features
 
-    block_upsample = []
+    block_to_up = []
 
     # the middle denseblock
 
     for j in range (LAYERS_PER_BLOCK[NUMPOOL]):
-        l = BN_Relu_Conv(stack, GROWTH_RATE, keep_prob= 0.8, BN_FLAG= BN_FLAG, kernel_size= 3)
-        block_upsample.append(l)
+        l = BN_Relu_Conv(stack, GROWTH_RATE, keep_prob= 0.8, BN_FLAG= BN_FLAG, kernel_size= 3, name='MConv_{}'.format(j))
+        block_to_up.append(l)
         stack = tf.concat([stack, l], axis= 3, name= 'Concat')
 
     #-------------------up scale layers-----------------------------------------------------------
     for i in range(NUMPOOL):
         n_filters_keep = GROWTH_RATE * LAYERS_PER_BLOCK[NUMPOOL + i]
-        stack = up_layer(skip_list[i], block_to_up, n_filters_keep, kernel_size= 3, stride= 2)
+        stack = up_layer(skip_list[i], block_to_up, n_filters_keep, kernel_size= 3, stride= 2, name= 'Up_{}'.format(i))
 
         block_to_up = []
 
         for j in range(LAYERS_PER_BLOCK[NUMPOOL + i + 1]):
-            l =BN_Relu_Conv(stack, GROWTH_RATE, keep_prob= 0.8, BN_FLAG= BN_FLAG, kernel_size= 3)
+            l =BN_Relu_Conv(stack, GROWTH_RATE, keep_prob= 0.8, BN_FLAG= BN_FLAG, kernel_size= 3, name= 'UpConv_{}_{}'.format(i,j))
             block_to_up.append(l)
             stack = tf.concat([stack, l], axis= 3, name = 'Concat')
 
     #----------------------the last 1x1 conv layer-----------------------------------------------
-    conv = BN_Relu_Conv(stack, out_channel= CLASSNUM, keep_prob= 0.8, BN_FLAG= BN_FLAG, kernel_size= 1)
+    conv = BN_Relu_Conv(stack, out_channel= CLASSNUM, keep_prob= 0.8, BN_FLAG= BN_FLAG, kernel_size= 1, name= 'LastConv')
 
     #---------------------softmax layer-----------------------------------------------------------
-    with tf.name_scope('Argmax'):
+    with tf.variable_scope('Argmax'):
         annot_pred = tf.argmax(conv, axis= -1, name=  'Prediction')
 
     return conv, tf.expand_dims(annot_pred, axis= 3)
 
 
 def BN_Relu_Conv(in_put, out_channel, keep_prob, BN_FLAG, kernel_size = 3, name = 'BN_Relu_Conv'):
-    with tf.name_scope(name):
+    with tf.variable_scope(name):
         shap = in_put.get_shape()
         in_channel = shap[-1]
         res = tf.layers.batch_normalization(in_put, momentum= 0.9, training= BN_FLAG, name= 'BatchNorm')
         res = tf.nn.relu(res)
-        res = conv2d_layer(kernel_size, res, in_channel, out_channel, keep_prob= keep_prob)
+        res = conv2d_layer(kernel_size, res, in_channel, out_channel, keep_prob= keep_prob, name= name)
         return res
 
 def down_layer(in_put, out_channel, keep_prob, BN_FLAG, name = 'down_layer'):
-    with tf.name_scope(name):
+    with tf.variable_scope(name):
         shap = in_put.get_shape()
         in_channel = shap[-1]
-        res = BN_Relu_Conv(in_put, in_channel, out_channel, keep_prob, BN_FLAG, kernel_size=1)
+        res = BN_Relu_Conv(in_put, out_channel, keep_prob, BN_FLAG, kernel_size=1, name= name)
         res = pool2d_layer(res, ksize = 2, stride= 2)
 
         return res
@@ -90,12 +90,12 @@ def up_layer(skip_conn, block_to_up, n_filters_keep, kernel_size, stride, name= 
     '''
 
     l = tf.concat(block_to_up, axis= 3, name= 'Concat')
-    shap = l.get_shape()
+    shap = l.get_shape().as_list()
     in_channel = shap[3]
     out_channel = n_filters_keep
-    out_shape = tf.convert_to_tensor([shap[0], 2*shap[1], 2*shap[2], shapp[3]])
-    with tf.name_scope(name):
-        deconv_weights, deconv_bias = get_var_transpose(kernel_size, in_channel, out_channel)
+    out_shape = tf.convert_to_tensor(list([shap[0], 2*shap[1], 2*shap[2], shap[3]]))
+    with tf.variable_scope(name):
+        deconv_weights, deconv_bias = get_var_transpose(kernel_size, in_channel, out_channel, name)
         l = tf.nn.conv2d_transpose(l, deconv_weights, out_shape, strides= [1, stride, stride, 1], padding= 'SAME')
         l = tf.nn.bias_add(l, deconv_bias)
         l = tf.concat([l, skip_conn], axis= 3, name= 'Concat')
@@ -103,16 +103,16 @@ def up_layer(skip_conn, block_to_up, n_filters_keep, kernel_size, stride, name= 
     return l
 
 def conv2d_layer(kernel_size, in_put, in_channel, out_channel, c_stride = 1, c_rate = 1, keep_prob = 0.8, name = None):
-    with tf.name_scope(name= name):
-        conv_weights, conv_bias = get_var(kernel_size, in_channel, out_channel)
-        conv = tf.nn.conv2d(in_put, conv_weights, stride = [1, c_stride, c_stride, 1], 
-                            padding= 'SAME', dialations= [1, c_rate, c_rate, 1])
+    with tf.variable_scope(name):
+        conv_weights, conv_bias = get_var(kernel_size, in_channel, out_channel, name)
+        conv = tf.nn.conv2d(in_put, conv_weights, strides = [1, c_stride, c_stride, 1],
+                            padding= 'SAME', dilations= [1, c_rate, c_rate, 1])
         conv = tf.nn.bias_add(conv, conv_bias)
         conv = tf.nn.dropout(conv, keep_prob)
 
         return conv
 
-def get_var(kernel_size, in_channel, out_channel):
+def get_var(kernel_size, in_channel, out_channel, name= None):
     weights = tf.get_variable('w', shape=[kernel_size, kernel_size, in_channel, out_channel],
                               initializer= tf.contrib.layers.xavier_initializer())
     bias = tf.get_variable('b', [out_channel], initializer=tf.constant_initializer(value=0.0, dtype=tf.float32))
@@ -125,7 +125,7 @@ def pool2d_layer(in_put, ksize, stride):
 
     return res
 
-def get_var_transpose(kernel_size, in_channel, out_channel):
+def get_var_transpose(kernel_size, in_channel, out_channel, name= None):
     weights = tf.get_variable('w', shape=[kernel_size, kernel_size, out_channel, in_channel],
                               initializer=tf.contrib.layers.xavier_initializer())
 
@@ -136,7 +136,7 @@ def get_var_transpose(kernel_size, in_channel, out_channel):
 
 
 #def bottleneck_layer(in_put, growth_rate, BN_FLAG, keep_prob= 0.8, name = None):
-#     with tf.name_scope(name):
+#     with tf.variable_scope(name):
 #         shap = in_put.get_shape()
 #         in_channel = shap[-1]
 #         res = BN_Relu_Conv(in_put, growth_rate, growth_rate*4, keep_prob, BN_FLAG, kernel_size= 1)
@@ -146,7 +146,7 @@ def get_var_transpose(kernel_size, in_channel, out_channel):
 #         return res
 #
 # def dense_block(in_put, n_layers, growth_rate, BN_FLAG, keep_prob, name):
-#     with tf.name_scope(name):
+#     with tf.variable_scope(name):
 #         layer_concat = list()
 #         layer_concat.append(in_put)
 #
@@ -162,7 +162,7 @@ def get_var_transpose(kernel_size, in_channel, out_channel):
 #         return res
 
 # def up_layer(in_put, out_shape, in_channel, out_channel, kernel_size, stride, c_rate= 1, name= 'up_layer'):
-#     with tf.name_scope(name):
+#     with tf.variable_scope(name):
 #         deconv_weights, deconv_bias = get_var_transpose(kernel_size, in_channel, out_channel)
 #         deconv = tf.nn.conv2d_transpose(in_put, deconv_weights, out_shape, strides= [1, stride, stride, 1],
 #                                         padding = 'SAME')
--- ISSeg/train_test_run_2d.py
+++ ISSeg/train_test_run_2d.py
@@ -5,6 +5,7 @@
 
 import net.UNet_2D as UNet_2D
 import net.NetDeepLab_2d as NetDeepLab_2d
+import net.dense_fc as dense_fc
 import utils.utils_fun as utils
 import net.LossPy as LossPy
 import os
@@ -27,12 +28,11 @@ IMAGE_CHANNEL = 5
 
 LEARNING_RATE = 1e-3
 WEIGHT_DECAY = 1e-4
-MAX_ITERATION = 8000
-ITER_PER_EPOCH = 70
-STEPINTERVAL = 8000
+MAX_ITERATION = 50000
+ITER_PER_EPOCH = 2100
+STEPINTERVAL = 50000
 CLASSNUM = 2
-TRAIN_BATCHSIZE = 32
-
+TRAIN_BATCHSIZE = 1
 
 
 def training(lr, loss_val, va_list):
@@ -44,19 +44,23 @@ def training(lr, loss_val, va_list):
 
 def FCNX_run():
     with tf.name_scope('inputs'):
-        annotation = tf.placeholder(tf.int32, shape=[ None, None, None, None], name='annotation')   # shape BHWC
-        image = tf.placeholder(tf.float32, shape=[ None, None, None, None], name='image')           # shape BHWC
+        annotation = tf.placeholder(tf.int32, shape=[TRAIN_BATCHSIZE, IMAGE_WIDTH, IMAGE_HEIGHT, 1], name='annotation')   # shape BHWC
+        image = tf.placeholder(tf.float32, shape=[TRAIN_BATCHSIZE, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNEL], name='image')           # shape BHWC
 
     bn_flag = tf.placeholder(tf.bool)
     keep_prob = tf.placeholder(tf.float32)
     train_batchsize = tf.placeholder(tf.int32)
 
-    #logits, pred_annot, _,_ = UNet_2D.build_unet(tensor_in= image, BN_FLAG= bn_flag, BATCHSIZE= train_batchsize, CLASSNUM= CLASSNUM, keep_porb= keep_prob, in_channels= IMAGE_CHANNEL)
+    #logits, pred_annot, _,_ = UNet_2D.build_unet(tensor_in= image, BN_FLAG= bn_flag, BATCHSIZE= train_batchsize,
+                                                # CLASSNUM= CLASSNUM, keep_porb= keep_prob, in_channels= IMAGE_CHANNEL)
 
-    logits, pred_annot = NetDeepLab_2d.LITS_DLab(tensor_in = image, BN_FLAG = bn_flag, BATCHSIZE = train_batchsize,
-                                                 CLASSNUM = CLASSNUM, KEEPPROB = keep_prob, IMGCHANNEL = IMAGE_CHANNEL)
+    # logits, pred_annot = NetDeepLab_2d.LITS_DLab(tensor_in = image, BN_FLAG = bn_flag, BATCHSIZE = train_batchsize,
+    #                                              CLASSNUM = CLASSNUM, KEEPPROB = keep_prob, IMGCHANNEL = IMAGE_CHANNEL)
 
-    with tf.name_scope('loss'):
+    logits, pred_annot = dense_fc.build_dense_fc(tensor_in= image, BN_FLAG= bn_flag, BATCHSIZE= train_batchsize,
+                                                 CLASSNUM= CLASSNUM,  IMGCHANNEL= IMAGE_CHANNEL,keep_prob= keep_prob)
+
+    with tf.variable_scope('loss'):
         class_weight = tf.constant([0.15,1])
         #loss_reduce = LossPy.cross_entropy_loss(pred= logits, ground_truth= annotation, class_weight= class_weight)
         # l2_loss = [WEIGHT_DECAY * tf.nn.l2_loss(v) for v in tf.trainable_variables() if 'w' in v.name]
@@ -67,7 +71,11 @@ def FCNX_run():
         #loss_reduce = LossPy.dice_sqaure(pred = logits, ground_truth= annotation)
         tf.summary.scalar('loss', loss_reduce)
 
-    with tf.name_scope('trainOP'):
+    with tf.variable_scope('valid_IOU'):
+        iou = tf.placeholder(tf.float32)
+        tf.summary.scalar('IOU', iou)
+
+    with tf.variable_scope('trainOP'):
         LRate = tf.placeholder(tf.float32)
         trainable_vars = tf.trainable_variables()
         train_op = training(LRate, loss_reduce, trainable_vars)
@@ -87,29 +95,18 @@ def FCNX_run():
         saver = tf.train.Saver()
 
         global LEARNING_RATE
-
+        meanIOU = 0.001
         for itr in range(MAX_ITERATION):
             vol_batch, seg_batch = utils.get_data_train_2d(trainPath, batchsize= TRAIN_BATCHSIZE)
             vol_shape = vol_batch.shape
             print(vol_shape)
+
             #----------------------changed------------------------------------------------------
-            if (itr + 1) % 1000 == 0:
+            if (itr + 1) % 10000 == 0:
                 LEARNING_RATE = LEARNING_RATE * 0.90
                 print('learning_rate:',LEARNING_RATE)
-
-            feed = {LRate: LEARNING_RATE, image: vol_batch, annotation: seg_batch, bn_flag: True, keep_prob: 0.8, train_batchsize: TRAIN_BATCHSIZE}
-            sess.run(train_op, feed_dict= feed)
-            train_loss_print, summary_str = sess.run([loss_reduce, merge_op], feed_dict=feed)
-            print(itr, vol_batch.shape)
-            print('loss:', train_loss_print)
-            writer.add_summary(summary_str, itr)
-
-            if (itr + 1) % STEPINTERVAL == 0:
-                saver.save(sess, resultPath + 'ckpt/modle', global_step= (itr+1))
-
-
-            #-------------------------validation with IOU each 10 epoch------------------------------------------------------
-            if (itr + 1) % (ITER_PER_EPOCH * 10) == 0:
+            # -------------------------validation with IOU each 10 epoch------------------------------------------------------
+            if itr % (ITER_PER_EPOCH * 5) == 0:
                 test_dirs = os.listdir(testPath + '/vol/')
                 one_pred_or_label = one_label_and_pred = 0
 
@@ -117,7 +114,7 @@ def FCNX_run():
                     vol_batch, seg_batch = utils.get_data_test_2d(testPath, t_dir)
                     test_feed = {image: vol_batch, annotation: seg_batch, bn_flag: False, keep_prob: 1,
                                  train_batchsize: 1}
-                    test_pred_annotation = sess.run([pred_annot, merge_op], feed_dict=test_feed)
+                    test_pred_annotation = sess.run(pred_annot, feed_dict=test_feed)
                     label_batch = np.squeeze(seg_batch).astype(np.uint8)
                     pred_batch = np.squeeze(test_pred_annotation).astype(np.uint8)
                     label_bool = (label_batch == 1)
@@ -127,11 +124,19 @@ def FCNX_run():
                     one_pred_or_label = one_pred_or_label + np.count_nonzero(union)
                     one_label_and_pred = one_label_and_pred + np.count_nonzero(intersection)
 
-                IOU = one_label_and_pred / (one_pred_or_label+1e-4)
+                meanIOU = one_label_and_pred / (one_pred_or_label + 1e-4)
+                print(meanIOU)
+            #------------------------------------------------------------------------------------------------------------------------------
 
-                tf.summary.scalar('IOU', IOU)
+            feed = {LRate: LEARNING_RATE, iou: meanIOU, image: vol_batch, annotation: seg_batch, bn_flag: True, keep_prob: 0.8, train_batchsize: TRAIN_BATCHSIZE}
+            sess.run(train_op, feed_dict= feed)
+            train_loss_print, summary_str = sess.run([loss_reduce, merge_op], feed_dict=feed)
+            print(itr, vol_batch.shape)
+            print('loss:', train_loss_print)
+            writer.add_summary(summary_str, itr)
 
-
+            if (itr + 1) % STEPINTERVAL == 0:
+                saver.save(sess, resultPath + 'ckpt/modle', global_step= (itr+1))
 #-------------------------------------Test Test Test Test-------------------------------------------------------------------------------
             if itr == (MAX_ITERATION - 1):
                 print('End training:{}'.format(datetime.datetime.now()))
